e539eab3c14ba922f6b13b0b7f7685f2ecd44036
MuhammedZeki <muhammed.codes@gmail.com>
2025-10-01 22:49:53 +0300

    dev

---
+++ letterChange.js
@@ -0,0 +1,12 @@
+const letterChange=(str)=>{
+    let newArr = str.toLowerCase().replace(/[a-z]/gi, (char)=>{
+        if(char === "z"){
+            return a
+        }else{
+            return String.fromCharCode(char.charCodeAt() +1)
+        }
+    })
+    let vowelCapitalize = newArr.replace(/a|e|i|o|u/gi, char => char.toUpperCase())
+    return vowelCapitalize
+}
+console.log(letterChange("merhabalar"))
